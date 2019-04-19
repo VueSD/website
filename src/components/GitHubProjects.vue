f4212284a33a058dacdd143d127d60bbe5a7bf38
Austin Gil <austin@stegosource.com>
2019-04-18 20:41:02 -0700

    April workshop

---
+++ src/components/GitHubProjects.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="project in projects" :key="project.id">
+      {{ project.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import API from "@/api";
+
+export default {
+  data: () => ({
+    projects: []
+  }),
+
+  async mounted() {
+    this.projects = await API.githubProjects("VueSD");
+  }
+};
+</script>
+
+<style>
+</style>
